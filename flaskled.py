973265a8bb0fa3c08ff8740a88c04b04690494b0
Abhi Velaga <abhinav.velaga@gmail.com>
2020-06-13 20:25:53 -0500

    working hue endpoint

---
+++ flaskled.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+app = Flask(__name__)
+from smbus import SMBus
+
+addr = 0x8 # bus address
+bus = SMBus(1)
+
+@app.route("/")
+def home():
+	hue = request.args.get('hue', default = 0, type = int)
+	bus.write_byte(addr,hue)
+	return "set hue to " + str(hue)
+
+if __name__ == '__main__':
+    app.run(debug=True)
