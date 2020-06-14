43db8a647b4c5971e28f44417bb1c450b1acb81f
Abhi Velaga <abhinav.velaga@gmail.com>
2020-06-13 20:49:00 -0500

    renamed endpoint

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ from smbus import SMBus
 addr = 0x8 # bus address
 bus = SMBus(1)
 
-@app.route("/")
-def home():
+@app.route("/hue")
+def hue():
 	hue = request.args.get('hue', default = 0, type = int)
 	bus.write_byte(addr,hue)
 	return "set hue to " + str(hue)
